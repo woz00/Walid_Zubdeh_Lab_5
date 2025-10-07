ebd77ba1064feca944caafb1cc4ce626537a47f5
woz00 <woz00@mail.aub.edu>
2025-10-08 01:20:00 +0300

    REST API Implementation

--- main/User.py
+++ main/User.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 import sqlite3
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 def connect_to_db():
     conn = sqlite3.connect('database.db')
@@ -130,3 +132,33 @@ def delete_user(user_id):
     return message
 
 connect_to_db()
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/users', methods=['GET'])
+def api_get_users():
+    return jsonify(get_users())
+
+@app.route('/api/users/<user_id>', methods=['GET'])
+def api_get_user(user_id):
+    return jsonify(get_user_by_id(user_id))
+
+@app.route('/api/users/add', methods = ['POST'])
+def api_add_user():
+    user = request.get_json()
+    return jsonify(insert_user(user))
+
+@app.route('/api/users/update', methods = ['PUT'])
+def api_update_user():
+    user = request.get_json()
+    return jsonify(update_user(user))
+
+@app.route('/api/users/delete/<user_id>', methods = ['DELETE'])
+def api_delete_user(user_id):
+    return jsonify(delete_user(user_id))
+
+if __name__ == "__main__":
+    #app.debug = True
+    #app.run(debug=True)
+    app.run() #run app
